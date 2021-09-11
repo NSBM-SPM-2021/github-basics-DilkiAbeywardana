19e7b2eb3f3382aeeef41b61140ab4bdf5cf2ef1
DilkiAbeywardana <dilkiabeywardana16@gmail.com>
2021-09-11 18:43:21 +0530

    New Commit

---
+++ src/Pages/Books.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Books.css';
+
+function Books() {
+  return (
+    <div className="Books">
+      <header className="Books-header">
+        
+        <p>
+             <h1>Books</h1>
+             <h5>~ Read It .. Love It .. Return It ~</h5>
+                 <i>This is Our Books Page & Now You Can See What Are The Books Available In Our Library..</i>
+        </p>
+           
+      </header>
+    </div>
+  );
+}
+
+
+export default Books;
