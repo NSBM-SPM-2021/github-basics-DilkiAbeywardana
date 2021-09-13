4be26e9aa114baf028415c1af0d2a50ff9439cd8
DilkiAbeywardana <dilkiabeywardana16@gmail.com>
2021-09-13 20:05:14 +0530

    New Commit

--- src/Pages/Members.js
+++ src/Pages/Members.js
@@ -2,18 +2,42 @@ import React from 'react';
 import './Members.css';
 
 function Members() {
-  
+
+
+
   return (
     
     <div className="Members">
       <header className="Members-header">
-        
-        <p>
-          <h1>Members</h1>
-              <h5><i>~ Always Be active ~</i></h5>
-                <i>This Is Our Member's Page & You Can Add Member To Our Library Using This Form.</i>
-        </p>
-      
+         <h1>Members</h1>
+         <h5>~ Always Be Active ~</h5>
+         <i>This Is Our Members' Page & Now, You Can Add Memeber Using This Form</i>
+         <br></br>
+         Add Member Form
+         <form>
+           <label>
+               Member_ID:
+              <input type="text" name="Member_ID" />
+            </label>
+               <br></br>
+            <label>
+               Name:
+              <input type="text" name="Name" />
+            </label>
+               <br></br>
+            <label>
+               Phone_Number:
+               <input type="text" name="Phone_Number" />
+            </label>
+                <br></br>
+            <label>
+               Email:
+                <input type="text" name="Email" />
+            </label>
+                <br></br>
+            <input type="submit" value="Submit" />
+         </form>
+
       </header>
     </div>
   );
